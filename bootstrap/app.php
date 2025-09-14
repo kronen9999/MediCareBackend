f4f38e34a8b96de245048a4f6ac521d0cdd2bf89
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-14 17:19:53 -0600

    Se cambio la ruta completa y se agregaron la rutas de familiares de registro y login para exepcion del token

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens([
-           'Familiares/*'
+            'Familiares/Registro',
+           'Familiares/Login',
+
         ]);
         //
     })
