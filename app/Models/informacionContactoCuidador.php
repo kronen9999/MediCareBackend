6b0a3a3a300ed380b4eb00f7a5f3f9f1dcdb7209
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-15 16:30:39 -0600

    Se ha agregado la relacion entre el modelo de cuidadores y el modelo de su informacion de contacto

--- app/Models/informacionContactoCuidador.php
+++ app/Models/informacionContactoCuidador.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\cuidadores;
 
 class informacionContactoCuidador extends Model
 {
-    //
+   protected $table="informacioncontactocuidador";
+
+   public function cuidador()
+   {
+       return $this->belongsTo(cuidadores::class, 'IdCuidador', 'IdCuidador');
+   }
+
 }
