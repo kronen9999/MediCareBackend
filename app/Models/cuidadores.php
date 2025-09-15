c02fb8306bdb416d89f1ae7f560aa72db9126408
kronen9999 <stacica22eneronkio@gmail.com>
2025-09-15 14:35:33 -0600

    Se han agregado mutadores y accesores para el modelo

--- app/Models/cuidadores.php
+++ app/Models/cuidadores.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class cuidadores extends Model
@@ -9,6 +10,37 @@ class cuidadores extends Model
     protected $table = 'cuidadores';
     protected $primaryKey = 'IdCuidador';
     public $timestamps = false;
+
+    public function Nombre():Attribute{
+        return Attribute::make(
+            get: fn($value) => ucfirst(strtolower($value)),
+            set: fn($value) => ucfirst(strtolower($value)),
+        );
+    }
+     public function ApellidoM():Attribute{
+        return Attribute::make(
+            get: fn($value) => ucfirst(strtolower($value)),
+            set: fn($value) => ucfirst(strtolower($value)),
+        );
+    }
+     public function ApellidoP():Attribute{
+        return Attribute::make(
+            get: fn($value) => ucfirst(strtolower($value)),
+            set: fn($value) => ucfirst(strtolower($value)),
+        );
+    }
+
+    public function CorreoE():Attribute{
+        return Attribute::make(
+            get: fn($value) => strtolower($value),
+            set: fn($value) => strtolower($value),
+        );
+    }
+
+    public function Contrasena():Attribute{
+        return Attribute::make(
+            set: fn($value) => bcrypt($value),);
+    }
 
 
     // Definir la relación con el modelo familiares
